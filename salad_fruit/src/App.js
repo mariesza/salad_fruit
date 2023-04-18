a43312d9826b9242147334d97728cbfc59e5d532
Marie Szabelski <marie.szabelski@gmail.com>
2023-04-18 12:21:11 +0200

    TD1 salade de fruit - terminé

---
+++ salad_fruit/src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import FruitsMaster from './components/FruitsMaster';
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+
+      <FruitsMaster/>
+
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ salad_fruit/src/components/FruitsMaster.js
@@ -0,0 +1,11 @@
+
+function FruitsMaster(){
+    return (
+    <div className ="MasterFruit">
+
+    <img src='/images/fruit-salad.png' alt="Salade de fruit" />
+    
+    </div>
+    )
+}
+export default FruitsMaster;
